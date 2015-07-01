509f37df79ca79866b97223893c73bcffe89167a
Julien Dubois <julien@happyculture.coop>
2015-07-01 17:11:13 +0200

    Simulate a call to the API and its response.

--- tenon/tenon.js
+++ tenon/tenon.js
@@ -1,8 +1,8 @@
 /**
- * jQuery behaviors for platform notification feeds.
+ * jQuery behaviors for tenon reports.
  */
 (function ($) {
-  Drupal.behaviors.tenon_notifications = {
+  Drupal.behaviors.tenon_reports = {
     attach: function (context, settings) {
       // Setup.
       var items = [];
@@ -15,89 +15,100 @@
       if (typeof hopscotch == 'undefined') {
         return;
       }
+      // Sets up the DOM elements.
+      // @TODO: Plug the persistent reports.
+      $(menuLinkSel).append($("<span id='tenon-report-summary-count'>1</span>"));
 
-      // Prepare data for the API request.
-      var i = 0;
-      var data = '';
-      var api_parameters = {
-        'key': settings.tenon.key,
-        'url': settings.tenon.url,
-        'appId': settings.tenon.appId,
-        'projectID': settings.tenon.projectID,
-        'certainty': settings.tenon.certainty,
-        'importance': settings.tenon.importance,
-        'level': settings.tenon.level,
-        'priority': settings.tenon.priority,
-        'ref': settings.tenon.ref,
-        'store': settings.tenon.store,
-        'uaString': settings.tenon.uaString,
-        'viewPortHeight': settings.tenon.viewPortHeight,
-        'viewPortWidth': settings.tenon.viewPortWidth
-      };
+      // Adds our tour overlay behavior with desired effects.
+      $('a[data-tenon="page_report"]').click(function (e) {
+        // Close the drawer when we generate the report.
+        Drupal.cp_toolbar.drawer_close();
 
-      for (var parameter in api_parameters ) {
-        var separator = '';
-        if (i++ != 0) {
-          separator = '&';
-        }
-        data += separator + parameter + '=' + encodeURIComponent(api_parameters[parameter]);
-      }
+        $('html, body').animate({scrollTop: 0}, '500', 'swing', function () {
+          var hopscoth_selector = '.hopscotch-bubble';
+          $(hopscoth_selector).addClass('animated');
 
-      // Do the actual call.
-      var url = 'http://tenon.io/api/';
-      var options = {
-        'headers': {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        'method': 'POST',
-        'data': data
-      };
-      //$.post(url, options);
+          var items = tenon_notification_generate_report(settings.tenon);
 
-      // Format the report content.
-      var report_id = 13456;
-      var entry = {
-        content: '<p>3 issues</p>',
-        link: 'https://tenon.io/report.php?id=' + report_id,
-        title: 'Your page accessibility report'
-      };
-      var item = tenon_notification_build_item(entry);
-      items.push(item);
+          // Sets up the tour object with the loaded feed item steps.
+          var tour = {
+            showPrevButton: true,
+            scrollTopMargin: 100,
+            id: "tenon-notifications",
+            steps: items
+          };
+          hopscotch.startTour(tour);
 
-      // If there are items to display in a hopscotch tour...
-      if (items.length) {
-        // Sets up the DOM elements.
-        // @TODO: Plug the persistent reports.
-        $(menuLinkSel).append($("<span id='tenon-report-summary-count'>1</span>"));
-        
-        // Sets up the tour object with the loaded feed item steps.
-        var tour = {
-          showPrevButton: true,
-          scrollTopMargin: 100,
-          id: "tenon-notifications",
-          steps: items,
-          onEnd: function () {
-            //os_tour_notifications_count(-1);
-            //os_tour_notifications_read_update();
-          }
-        };
+          // Temporary solution to simulate a call to the API.
+          setTimeout(function () {
+            $('.tenon-notifications .hopscotch-content .description').html('OK');
+          }, 2000);
 
-        // Adds our tour overlay behavior with desired effects.
-        $('a[data-tenon="page_report"]').click(function (e) {
-          // Close the drawer when we generate the report.
-          Drupal.cp_toolbar.drawer_close();
-
-          $('html, body').animate({scrollTop: 0}, '500', 'swing', function () {
-            $('.hopscotch-bubble').addClass('animated');
-            hopscotch.startTour(tour);
-
-            // Removes animation for each step and let us to target just this tour in CSS rules.
-            $('.hopscotch-bubble').removeClass('animated').addClass('tenon-notifications');
-          });
+          // Removes animation for each step and let us to target just this tour in CSS rules.
+          $(hopscoth_selector).removeClass('animated').addClass('tenon-notifications');
         });
-      }
+      });
     }
   };
+
+  /**
+   * Helper to query the API and generate the report content.
+   *
+   * @param settings
+   *   Drupal settings.
+   *
+   * @returns
+   *   Array of data for the Tour module.
+   */
+  function tenon_notification_generate_report(settings) {
+    // Prepare data for the API request.
+    var i = 0;
+    var data = '';
+    var api_parameters = {
+      'key': settings.key,
+      'url': settings.url,
+      'appId': settings.appId,
+      'projectID': settings.projectID,
+      'certainty': settings.certainty,
+      'importance': settings.importance,
+      'level': settings.level,
+      'priority': settings.priority,
+      'ref': settings.ref,
+      'store': settings.store,
+      'uaString': settings.uaString,
+      'viewPortHeight': settings.viewPortHeight,
+      'viewPortWidth': settings.viewPortWidth
+    };
+
+    for (var parameter in api_parameters ) {
+      var separator = '';
+      if (i++ != 0) {
+        separator = '&';
+      }
+      data += separator + parameter + '=' + encodeURIComponent(api_parameters[parameter]);
+    }
+
+    // Do the actual call.
+    var url = 'http://tenon.io/api/';
+    var options = {
+      'headers': {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      'method': 'POST',
+      'data': data
+    };
+    //$.post(url, options);
+
+    // Format the report content.
+    var report_id = 13456;
+    var entry = {
+      content: '<p>Report generation in progress. <br /> <img src="' + settings.moduleBasePath + '/throbber.gif" /></p>',
+      link: 'https://tenon.io/report.php?id=' + report_id,
+      title: 'Your page accessibility report'
+    };
+    // Tour expects an array of items.
+    return [tenon_notification_build_item(entry)];
+  }
 
   /**
    * Format Tour content.
